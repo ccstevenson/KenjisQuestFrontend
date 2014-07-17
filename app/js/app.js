88df44a206c48289929838e2df7e69556764226d
Michael Anderton <michael.anderton15@gmail.com>
2014-07-17 15:57:43 -0600

    Added restangular and allowed it to be installed through bower

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,12 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'restangular'
 ]).
-config(['$routeProvider', function($routeProvider) {
+config(['$routeProvider','RestangularProvider', function($routeProvider, RestangularProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
+  RestangularProvider.setBaseURL('http://localhost:8001');
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', ['$scope', function($scope, Restangular) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('MyCtrl2', ['$scope', function($scope, Restangular) {
 
   }]);
