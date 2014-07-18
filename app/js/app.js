334033829b19e0e8244e0a3bd311cef887b22b05
Christopher Stevenson <ccstevenson@live.com>
2014-07-18 17:07:03 -0600

    Added angular-seed hidden files.

--- app/js/app.js
+++ app/js/app.js
@@ -7,12 +7,10 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers',
-  'restangular'
+  'myApp.controllers'
 ]).
-config(['$routeProvider','RestangularProvider', function($routeProvider, RestangularProvider) {
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
-  RestangularProvider.setBaseURL('http://localhost:8001');
 }]);
