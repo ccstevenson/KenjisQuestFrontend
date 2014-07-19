43674caacaa8d042e03b4b145fb8c9004932934a
Christopher Stevenson <chris@Christophers-Mac-Pro.local>
2014-07-18 19:50:27 -0600

    Three-way data binding now working.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,19 +3,36 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+    .controller('MyCtrl1', ['$scope', function ($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+    }])
+    .controller('CharacterCreationController', ['$scope', function ($scope) {
 
-  }])
-  .controller('BattleatronicCtrl', ['$scope', 'FirebaseService', function($scope, FirebaseService) {
-      $scope.user = "Guest " + Math.round(Math.random()*101);
-      $scope.game = FirebaseService;
+        $scope.addCharacter = function () {
+            $scope.characters.$add({from: $scope.user, content: $scope.character});
+            $scope.message = "";
+        };
 
-      $scope.addMessage = function() {
-        $scope.messages.$add({from: $scope.user, content: $scope.message});
-        $scope.message = "";
-      };
-        
-  }]);
+    }])
+    .controller('BattleatronicCtrl', ['$scope', 'GameService', function ($scope, GameService) {
+        $scope.game = {};
+
+        $scope.game.characters = {'Andrew': 'tank', 'Chris': 'priest'};
+
+        GameService.$bind($scope, "game");
+
+
+
+//        $scope.user = "Guest " + Math.round(Math.random() * 101);
+//        $scope.game = GameService;
+////        $scope.$add({game: null});
+
+        // This code works.
+//        $scope.user = "Guest " + Math.round(Math.random() * 101);
+
+//        $scope.addMessage = function () {
+//            $scope.messages.$add({from: $scope.user, content: $scope.message});
+//            $scope.message = "";
+//        };
+
+    }]);
