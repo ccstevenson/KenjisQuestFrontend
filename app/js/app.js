f27a121af23bffcae628ece5bf15dd45f716408d
Michael Anderton <michael.anderton15@gmail.com>
2014-07-22 13:34:55 -0600

    Got youtube search working, added ability to add videos to playlist

--- app/js/app.js
+++ app/js/app.js
@@ -9,10 +9,19 @@ angular.module('myApp', [
     'myApp.directives',
     'myApp.controllers',
     'firebase',
-    'ui.bootstrap'
+    'ui.bootstrap',
+    'LocalStorageModule'
 ]).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/battleatronic', {templateUrl: 'partials/battleatronic.html', controller: 'BattleatronicCtrl'});
-        $routeProvider.when('/soundboard', {templateUrl: 'partials/soundboard.html', controller: 'SoundboardCtrl'});
+        $routeProvider.when('/soundboard', {templateUrl: 'partials/soundboard.html', controller: 'VideosController'});
         $routeProvider.otherwise({redirectTo: '/battleatronic'});
     }]);
+
+
+angular.module('myApp').run(function () {
+  var tag = document.createElement('script');
+  tag.src = "http://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
